a3eb5f1b2d73c80a7c4ce31a8f5845732b36afca
Nishant <ncnishant9868@gmail.com>
2019-03-05 22:13:36 +0530

    eyedetection added

--- video_main.py
+++ video_main.py
@@ -3,6 +3,8 @@ import numpy
 
 face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+eye_classifier = cv2.CascadeClassifier('haarcascade_eye.xml')
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -11,6 +13,12 @@ while True:
     faces = face_classifier.detectMultiScale(gray,1.2,5)
     for (x,y,w,h) in faces:
         cv2.rectangle(img, (x,y),(x+w,y+h),(255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_img = img[y:y+h, x:x+w]
+        eyes = eye_classifier.detectMultiScale(roi_gray,1.2,5)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
